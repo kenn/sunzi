be519d810daa97e7483aa8b9bbdeccfa126078ae
Kenn Ejima <kenn.ejima@gmail.com>
2014-01-31 11:22:09 -0800

    Support Rainbow 2.0.0 as well as older versions. Fixes #26

--- lib/sunzi.rb
+++ lib/sunzi.rb
@@ -2,6 +2,10 @@ require 'thor'
 require 'rainbow'
 require 'yaml'
 
+# Starting 2.0.0, Rainbow no longer patches string with the color method by default.
+require 'rainbow/version'
+require 'rainbow/ext/string' unless Rainbow::VERSION < '2.0.0'
+
 module Sunzi
   autoload :Cli,        'sunzi/cli'
   autoload :Cloud,      'sunzi/cloud'
